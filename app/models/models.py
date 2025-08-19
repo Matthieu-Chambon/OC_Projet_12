c21f00d0754c32da0ffd379a3ffc5568de5e1cbe
Matthieu <matthieu.chambon44@gmail.com>
2025-08-19 13:51:07 +0200

    MAJ modèles

--- app/models/models.py
+++ app/models/models.py
@@ -31,7 +31,8 @@ class Employee(Base):
     
     role = relationship("Role", back_populates="employees")
     customers: Mapped[list["Customer"]] = relationship("Customer", back_populates="sale_contact", passive_deletes=True)
-    contracts : Mapped[list["Contract"]] = relationship("Contract", back_populates="employee")
+    contracts : Mapped[list["Contract"]] = relationship("Contract", back_populates="employee", passive_deletes=True)
+    events: Mapped[list["Event"]] = relationship("Event", back_populates="support_contact", passive_deletes=True)
 
     def __repr__(self):
         return f"<Employee(id={self.id}, employee_number='{self.employee_number}', first_name='{self.first_name}', last_name='{self.last_name}', email='{self.email}', role_id={self.role_id})>"
@@ -51,8 +52,8 @@ class Customer(Base):
     updated_at: Mapped[DateTime] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now(), nullable=False)
 
     sale_contact = relationship("Employee", back_populates="customers")
-    contracts : Mapped[list["Contract"]] = relationship("Contract", back_populates="customer")
-    
+    contracts : Mapped[list["Contract"]] = relationship("Contract", back_populates="customer", passive_deletes=True)
+
     def __repr__(self):
         return f"<Customer(id={self.id}, first_name='{self.first_name}', last_name='{self.last_name}', email='{self.email}', phone='{self.phone}', company='{self.company}', sale_contact_id='{self.sale_contact_id}')>"
 
@@ -61,20 +62,19 @@ class Contract(Base):
     __tablename__ = "contract"
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    customer_details: Mapped[str] = mapped_column(Text, nullable=False)
-    employee_id: Mapped[int] = mapped_column(ForeignKey("employee.id"), nullable=False)
-    customer_id: Mapped[int] = mapped_column(ForeignKey("customer.id"), nullable=False)
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customer.id", ondelete="CASCADE"), nullable=False)
+    sale_contact_id: Mapped[int] = mapped_column(ForeignKey("employee.id", ondelete="SET NULL"), nullable=True)
     total_amount: Mapped[float] = mapped_column(DECIMAL(10, 2), nullable=False)
     remaining_amount: Mapped[float] = mapped_column(DECIMAL(10, 2), nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)
     signed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)    
 
     customer = relationship("Customer", back_populates="contracts")
     employee = relationship("Employee", back_populates="contracts")
-    event = relationship("Event", back_populates="contract", uselist=False)
+    event = relationship("Event", back_populates="contract", uselist=False, passive_deletes=True)
     
     def __repr__(self):
-        return f"<Contract(id={self.id}, customer_id={self.customer_id}, employee_id={self.employee_id}, total_amount={self.total_amount}, remaining_amount={self.remaining_amount}, signed={self.signed})>"
+        return f"<Contract(id={self.id}, customer_id={self.customer_id}, sale_contact_id={self.sale_contact_id}, total_amount={self.total_amount}, remaining_amount={self.remaining_amount}, signed={self.signed})>"
     
     
 class Event(Base):
@@ -82,15 +82,17 @@ class Event(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
-    contract_id: Mapped[int] = mapped_column(ForeignKey("contract.id"), nullable=False)
+    contract_id: Mapped[int] = mapped_column(ForeignKey("contract.id", ondelete="CASCADE"), nullable=False)
+    support_contact_id: Mapped[int] = mapped_column(ForeignKey("employee.id", ondelete="SET NULL"), nullable=True)
     start_date: Mapped[DateTime] = mapped_column(TIMESTAMP, nullable=True)
     end_date: Mapped[DateTime] = mapped_column(TIMESTAMP, nullable=True)
     location: Mapped[str] = mapped_column(String(255), nullable=True)
     attendees: Mapped[int] = mapped_column(Integer, nullable=True)
-    note: Mapped[str] = mapped_column(Text, nullable=True)
+    note: Mapped[str] = mapped_column(Text(255), nullable=True)
     created_at: Mapped[DateTime] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)
     
     contract = relationship("Contract", back_populates="event")
-    
+    support_contact = relationship("Employee", back_populates="events")
+
     def __repr__(self):
-        return f"<Event(id={self.id}, name='{self.name}', contract_id={self.contract_id}, start_date={self.start_date}, end_date={self.end_date}, location='{self.location}', attendees={self.attendees})>"
+        return f"<Event(id={self.id}, name='{self.name}', contract_id={self.contract_id}, support_contact_id={self.support_contact_id}, start_date='{self.start_date}', end_date='{self.end_date}', location='{self.location}', attendees={self.attendees}, note='{self.note}')>"
